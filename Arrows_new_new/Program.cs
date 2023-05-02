7af9983347e69cad5f45bdff16cbaec7b12e70fb
vsesvit <sfrol91@gmail.com>
2023-05-02 22:46:23 +0200

    TODO: two modes of program

--- Arrows_new_new/Program.cs
+++ Arrows_new_new/Program.cs
@@ -7,25 +7,40 @@ class Program
 {
     public static void Main(string[] args)
     {
-        //TODO: two modes of program
-        // 1 - Random
-        // 2 - Manual, which already is used
-        // Use methods RunRandomApplicationMode, RunManualApplicationMode
-
+        Console.WriteLine("Do you want to calculate the price of arrow manual or random? Please, enter: 1 - for manual, 2 - for random");
+        string chooseWayCalculation = Console.ReadLine();
+        int.TryParse(chooseWayCalculation, out int calculation);
+        if (calculation == 1)
+        {
+            RunManualApplicationMode();
+        }
+        else if (calculation == 2)
+        {
+            RunRandomApplicationMode();
+        }
+        else
+        {
+            Console.WriteLine("Please, enter the correct values for choosing calculation");
+        }
+    }
+    //TODO: two modes of program
+    // 1 - Random
+    // 2 - Manual, which already is used
+    // Use methods RunRandomApplicationMode, RunManualApplicationMode
+    public static void RunManualApplicationMode()
+    {
+        //TODO: move here
         Console.WriteLine("What type of arrow`s head do you want? Please, choose: 1 - for steel, 2 - for wood, 3 - for odsidian");
         string chooseHead = Console.ReadLine();
-        //float arrowhead_type = Convert.ToSingle(chooseHead);
         int.TryParse(chooseHead, out int chooseHeadNumber);
         Console.WriteLine("What type of arrow`s fletch do you want? Please, choose: 1 - for plastic, 2 - for turkey, 3 - for goose");
         string chooseFletch = Console.ReadLine();
-        //float fletching_type = Convert.ToSingle(chooseFletch);
         int.TryParse(chooseFletch, out int chooseFletchNumber);
         Console.WriteLine("How long of arrow`s shaft do you want? Please, enter the length between 60 and 100 cm");
         string someN = Console.ReadLine();
-     //float length = Convert.ToSingle(someN);
 
         int.TryParse(someN, out int lengthForCalculation);
-        
+
         HeadType arrowhead_type;
         FletchingType fletching_type;
 
@@ -34,7 +49,6 @@ class Program
             ((lengthForCalculation >= 60) && (lengthForCalculation <= 100)))
 
         {
-
             switch (chooseHeadNumber)
             {
                 case (1):
@@ -46,7 +60,7 @@ class Program
                 case (3):
                     arrowhead_type = HeadType.Obsidian;
                     break;
-                
+
                 default:
                     arrowhead_type = HeadType.Steel;
                     Console.WriteLine("Please, enter the correct values for choosing arow`s head parametrs");
@@ -69,10 +83,9 @@ class Program
                     Console.WriteLine("Please, enter the correct values for choosing arow`s fletch parametrs");
                     break;
             }
-            var arrow = new Arrow (arrowhead_type, fletching_type, lengthForCalculation);
+            var arrow = new Arrow(arrowhead_type, fletching_type, lengthForCalculation);
             float result = arrow.GetCost();
-           Console.WriteLine(result); 
-
+            Console.WriteLine(result);
         }
         else
         {
@@ -80,26 +93,24 @@ class Program
         }
     }
 
-    public static void RunManualApplicationMode()
-    {
-        //TODO: move here
-    }
-    
+
+
     public static void RunRandomApplicationMode()
     //(length)
     {
 
-       // int length = 100;
-       // Random choice = new Random();
-       // int[] headArr = { (int)HeadType.Steel, (int)HeadType.Wood, (int)HeadType.Obsidian };
-       // int[] fletchArr = { (int)FletchingType.Plastic, (int)FletchingType.Turkey, (int)FletchingType.Goose };
+        int length = 100;
+        Console.WriteLine(length);
+        // Random choice = new Random();
+        // int[] headArr = { (int)HeadType.Steel, (int)HeadType.Wood, (int)HeadType.Obsidian };
+        // int[] fletchArr = { (int)FletchingType.Plastic, (int)FletchingType.Turkey, (int)FletchingType.Goose };
 
-       // int hIndex = choice.Next(headArr.Length);
-       // int fIndex = choice.Next(fletchArr.Length);
-       // Console.WriteLine(headArr[hIndex]);
-       // Console.WriteLine(fletchArr[fIndex]);
-       // Arrow arrowRandom = new Arrow(headArr[hIndex], fletchArr[fIndex], length);
-       // int sumRandom = headArr[hIndex] + fletchArr[fIndex] + length;
-       //Console.WriteLine(sumRandom);
+        // int hIndex = choice.Next(headArr.Length);
+        // int fIndex = choice.Next(fletchArr.Length);
+        // Console.WriteLine(headArr[hIndex]);
+        // Console.WriteLine(fletchArr[fIndex]);
+        // Arrow arrowRandom = new Arrow(headArr[hIndex], fletchArr[fIndex], length);
+        // int sumRandom = headArr[hIndex] + fletchArr[fIndex] + length;
+        //Console.WriteLine(sumRandom);
     }
 }
