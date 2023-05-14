636d708727ff23d89eadb062d2a79e7b2c53f6f4
Serhii Matviienko <smatvienko@outlook.com>
2023-05-14 12:23:46 +0300

    Add reference test for Trader>GetCost

---
+++ Test/TraderTests.cs
@@ -0,0 +1,25 @@
+﻿using Arrows;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    public class TraderTests
+    {
+        [Fact]
+        public void FactCheck()
+        {
+            var arrow = new Arrow(HeadType.Obsidian, FletchingType.Goose, 60);
+            var traderArrows = new Arrow[] {
+            new Arrow(HeadType.Obsidian, FletchingType.Goose, 60),
+            new Arrow(HeadType.Wood, FletchingType.Plastic, 60)
+        };
+            var trader = new Trader(traderArrows);
+
+            Assert.Equal(trader.GetCost(arrow), 11f);
+        }
+    }
+}
