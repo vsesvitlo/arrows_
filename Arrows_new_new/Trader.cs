819ebb2971f3b40b81b15b807918ce9f3bb78f32
vsesvit <sfrol91@gmail.com>
2023-05-14 10:36:37 +0200

    questions:for\loop

--- Arrows_new_new/Trader.cs
+++ Arrows_new_new/Trader.cs
@@ -18,52 +18,26 @@ namespace Arrows
         
 public bool HasArrow(HeadType arrowhead, FletchingType fletching, float leng)
             {
-                for (int i = 0; i < this.arrows.Length; i++)
+            for (int i = 0; i < this.arrows.Length; i++)
+            {
+              
 
+                for (int j = 0; j < arrows[i].Length; j++)
                 {
-                //arrows[i];
-                i++;
 
-                // for (int j = 0; j < 2; j++)
-                {
-                    foreach (Arrow j in arrows)
-                    {
-                        i == j;
-                    
-                }
-                   
-                {
-                    //Console.WriteLine(arrows[i, j]);
-
-                   // if (arrowhead == HeadType )
-                    {
-
-                    }
-                }
-                       // for (int j = 0; j < arrows.GetLength(1); j++)
-               // {
-                    //Console.WriteLine(arrows[i, j]);
-               // }
-                // for (int j = 0; j < arrows.GetLength(1); j++)
-                //{
-                    //Console.WriteLine(arrows[i, j]);
-                //}
-                    /*foreach (int i in arrows)
-                    {
-
-                    }*/
                 }
 
+            }
                     return false;
             }
 
-public float GetCost()
+/*public float GetCost()
             {
                 int headTypeArrResult = (int)this._arrowhead_type;
                 int fletchTypeArrResult = (int)this._fletching_type;
                 float sum = headTypeArrResult + fletchTypeArrResult + (_length * 0.05f);
                 return sum;
             }
-        }
+        }*/
     }
 
