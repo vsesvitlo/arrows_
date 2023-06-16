f5e609b1c49c30899f91477a06ff5202f242935d
vsesvit <sfrol91@gmail.com>
2023-06-16 09:12:41 +0200

    Tests

--- Test/ArrowTests.cs
+++ Test/ArrowTests.cs
@@ -9,79 +9,56 @@ public class ArrowTests
     {
         //TODO: one check in one method
         Arrow check = new Arrow(HeadType.Steel, FletchingType.Goose, 60);
-        var condition = check._length == 60;
-        Assert.True(condition);
+        Assert.Equal(60, check._length);
     }
 
     [Fact]
     public void TestLength100()
     {
         Arrow check = new Arrow(HeadType.Steel, FletchingType.Goose, 100);
-        var condition = check._length == 100;
-        Assert.True(condition);
+        Assert.Equal(100, check._length);
     }
 
     [Fact]
     public void TestLength90()
     {
         Arrow check = new Arrow(HeadType.Steel, FletchingType.Goose, 90);
-        var condition = check._length == 90;
-        Assert.True(condition);
+        Assert.Equal(90, check._length);
     }
     [Fact]
     public void TestHeadWood()
     {
         Arrow check = new Arrow(HeadType.Wood, FletchingType.Turkey, 96);
-        var condition = check._arrowhead_type == HeadType.Wood;
-        Assert.True(condition);
+        Assert.Equal(HeadType.Wood, check._arrowhead_type);
     }
     [Fact]
     public void TestHeadSteel()
     {
         Arrow check = new Arrow(HeadType.Steel, FletchingType.Turkey, 97);
-        var condition = check._arrowhead_type == HeadType.Steel;
-        Assert.True(condition);
+        Assert.Equal(HeadType.Steel, check._arrowhead_type);
     }
     [Fact]
     public void TestHeadObsidian()
     {
         Arrow check = new Arrow(HeadType.Obsidian, FletchingType.Turkey, 65);
-        var condition = check._arrowhead_type == HeadType.Obsidian;
-        Assert.True(condition);
+        Assert.Equal(HeadType.Obsidian, check._arrowhead_type);
     }
     [Fact]
     public void TestFletchPlactic()
     {
         Arrow check = new Arrow(HeadType.Obsidian, FletchingType.Plastic, 76);
-        var condition = check._fletching_type == FletchingType.Plastic;
-        Assert.True(condition);
+        Assert.Equal(FletchingType.Plastic, check._fletching_type);
     }
     [Fact]
     public void TestFletchTurkey()
     {
         Arrow check = new Arrow(HeadType.Obsidian, FletchingType.Turkey, 78);
-        var condition = check._fletching_type == FletchingType.Turkey;
-        Assert.True(condition);
+        Assert.Equal(FletchingType.Turkey, check._fletching_type);
     }
     [Fact]
     public void TestFletchGoose()
     {
         Arrow check = new Arrow(HeadType.Obsidian, FletchingType.Goose, 88);
-        var condition = check._fletching_type == FletchingType.Goose;
-        Assert.True(condition);
+        Assert.Equal(FletchingType.Goose, check._fletching_type);
     }
-    /*[Fact]
-    public void TestSum()
-    {
-        Arrow check = new Arrow(HeadType.Obsidian, FletchingType.Goose, 88);
-        var condition = check.GetCost() == 12.4f; // 5 + 3 + 88 * 0.05f; 
-        Assert.True(condition);
-    }
-    [Fact]
-    public void TestSum60()
-    {
-        Arrow check = new Arrow(HeadType.Obsidian, FletchingType.Goose, 60);
-        var condition = check.GetCost() == 11f; // 5 + 3 + 60 * 0.05f; 
-        Assert.True(condition);
-    }*/
 }
